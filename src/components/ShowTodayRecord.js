9742308fea0ba3efb2b16adfff4ebc38c2f383e8
MiraeKim <hufs202000512@gmail.com>
2024-06-29 07:41:05 +0900

    style: 하루기록 ui 수정

--- src/components/ShowTodayRecord.js
+++ src/components/ShowTodayRecord.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const Strong = styled.strong`
   font-weight: bold;
   font-size: 20px;
+  margin-right: 30px;
 `;
 
 
@@ -39,24 +40,25 @@ const ShowTodayRecord = () => {
           <div
             key={record.id}
             style={{
-              border: '1px solid #ccc',
-              padding: '10px',
+              padding: '10px 10px 10px 30px',
               margin: '10px 0',
+              borderRadius: '20px',
+              boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.10)',
             }}
           >
-            <p>
+            <p style={{"fontSize": "20px"}}>
               <Strong>일정 </Strong> {record.category}
-            </p>
-            <p>
+            </p >
+            <p style={{"fontSize": "20px"}}>
               <Strong>무엇 </Strong> {record.whatIs}
             </p>
-            <p>
+            <p style={{"fontSize": "20px"}}>
               <Strong>누구 </Strong> {record.whoIs}
             </p>
-            <p>
+            <p style={{"fontSize": "20px"}}>
               <Strong>어디 </Strong> {record.whereIs}
             </p>
-            <p>
+            <p style={{"fontSize": "20px"}}>
               <Strong>시간 </Strong> {record.whatTime}
             </p>
           </div>
